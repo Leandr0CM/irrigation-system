8d9642e9240163bd9fb7c9a83943b45279e3b6d4
Jorge Samuel Teixeira Jordão <116199476+JorgeSTJordao@users.noreply.github.com>
2024-05-28 19:45:50 -0300

    :+1:  Now register and login are fine

--- utils/create_db.py
+++ utils/create_db.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from models.user.roles import Role
+from models.user.users import User
 from models import *
 
 #trata-se de um módulo reutilizável (utilities)
@@ -8,8 +9,16 @@ def create_db(app: Flask):
         db.drop_all() #elimina o banco de dados
         db.create_all() #cria um novo a partir de dados novos
 
-        GLOBAL_IDENTIFIERS = ['admnistrador', "estatístico", "operador", "normal"]
+        GLOBAL_IDENTIFIERS = ["administrador", "estatístico", "operador", "normal"]
+        PRIMARY_USERS = {
+            "Jorge": ["1234", GLOBAL_IDENTIFIERS[0]],
+            "Leandro": ["1234", GLOBAL_IDENTIFIERS[1]],
+            "Joelton": ["1234", GLOBAL_IDENTIFIERS[2]]
+        }
 
         for identifier in GLOBAL_IDENTIFIERS:
             Role.insert_role(identifier)
 
+        for key, value in PRIMARY_USERS.items():
+            print(PRIMARY_USERS[key])
+            User.insert_user(key, PRIMARY_USERS[key][0], PRIMARY_USERS[key][1])
